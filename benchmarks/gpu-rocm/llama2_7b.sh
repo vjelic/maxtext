f0524e79c3e32c4fbc6e6a69115e3c077a04245c
Lorri Rao <zouyoo@outlook.com>
2025-03-18 13:37:22 -0700

    Add benchmarking script for rocm GPU To launch, you need to call it likez; IMAGE="rocm/jax-maxtext-training-private:20250317" llama2_7b.sh

---
+++ benchmarks/gpu-rocm/llama2_7b.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -x
+OUTPUT_DIR="$HOME/output"
+
+# Environment variables
+echo 'export XLA_FLAGS="--xla_gpu_enable_cublaslt=True --xla_gpu_graph_level=0 --xla_gpu_autotune_level=0 --xla_gpu_enable_latency_hiding_scheduler=TRUE --xla_gpu_all_gather_combine_threshold_bytes=8589934592 --xla_gpu_enable_all_gather_combine_by_dim=FALSE --xla_gpu_memory_limit_slop_factor=95"
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.967
+export LD_LIBRARY_PATH=/usr/local/lib/:/opt/rocm/lib:$LD_LIBRARY_PATH' > $OUTPUT_DIR/maxtext_env_7b.sh
+
+# Model Configuration
+echo 'base_config: "base.yml"
+run_name: "llama2_7b_training"
+base_output_directory: "./"
+hardware: "gpu"
+steps: 50
+model_name: "llama2-7b"
+enable_checkpointing: False
+attention: "cudnn_flash_te"
+log_period: 100
+  #inter-node parallelism strategy
+dcn_data_parallelism: -1
+dcn_fsdp_parallelism: 1
+  #intra-node parallelism strategy
+ici_fsdp_parallelism: 8
+ici_data_parallelism: 1
+remat_policy: "minimal_flash"
+use_iota_embed: True
+scan_layers: False
+async_checkpointing: False
+logits_dot_in_fp32: False
+profiler: ""
+dtype: "bfloat16"
+quantization: ""
+quantize_kvcache: False
+kv_quant_axis: "heads_and_dkv"
+kv_quant_dtype: "int8"
+weight_dtype: bfloat16
+checkpoint_is_quantized: False # Set to True if reading from a saved aqt quantized checkpoint
+max_target_length: 4096
+per_device_batch_size: 10
+hf_path: "parquet" 
+hf_train_files: "/home/amd/data/c4/en/partial-train/000*.parquet"
+dataset_type: "hf"
+tokenizer_path: "/home/amd/data/Llama-2-7b-hf/"' > $OUTPUT_DIR/llama2_7b_gpu.yml
+
+#If podman is available instead of docker, then you need this export otherwise
+#comment the below line and uncomment the line after that
+#export docker=podman
+docker=docker
+
+
+
+# get the test data
+pip install -U "huggingface_hub[cli]" > /dev/null
+export PATH=~/.local/bin/:$PATH
+huggingface-cli download meta-llama/Llama-2-7b-chat-hf --local-dir $HOME/data/Llama-2-7b-hf/ --cache-dir $HOME/data > /dev/null
+huggingface-cli download legacy-datasets/c4 --include "*.parquet" --repo-type dataset --local-dir $HOME/data/c4 --revision refs/convert/parquet --cache-dir $HOME/data > /dev/null
+
+
+$docker run --rm --privileged --network host --device /dev/dri --device /dev/kfd \
+  --cap-add=IPC_LOCK --volume /dev/infiniband:/dev/infiniband \
+  -v $HOME:$HOME -v $HOME/data:/home/amd/data --tmpfs /dev/shm:size=50G \
+  --mount type=bind,source=$OUTPUT_DIR,target=/workspace/maxtext/output \
+  -w /workspace/maxtext $IMAGE /bin/bash -c "
+        set -e
+        echo \"Running Llama-2-7b\"
+	echo '${IMAGE}'
+	cp $OUTPUT_DIR/maxtext_env_7b.sh .
+	cp $OUTPUT_DIR/llama2_7b_gpu.yml MaxText/configs/llama2_7b_gpu.yml
+        source maxtext_env_7b.sh	
+        python MaxText/train.py MaxText/configs/llama2_7b_gpu.yml  2>&1 |& tee -a llama2_7b.real.log
+    "
+
---
+++ benchmarks/gpu-rocm/llama3_70b.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+set -x
+OUTPUT_DIR="$HOME/output"
+
+# Environment variables
+echo 'export XLA_FLAGS="--xla_gpu_enable_cublaslt=True --xla_gpu_graph_level=0 --xla_gpu_autotune_level=0 --xla_gpu_enable_reduce_scatter_combine_by_dim=false --xla_gpu_reduce_scatter_combine_threshold_bytes=8589934592 --xla_gpu_all_reduce_combine_threshold_bytes=8589934592  --xla_gpu_all_gather_combine_threshold_bytes=137438953472 --xla_gpu_enable_all_gather_combine_by_dim=FALSE"
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.975
+export LD_LIBRARY_PATH=/usr/local/lib/:/opt/rocm/lib:$LD_LIBRARY_PATH' > $OUTPUT_DIR/maxtext_env_70b.sh
+
+
+# Model Configuration
+echo 'base_config: "base.yml"
+run_name: "llama3_70b_training"
+hardware: "gpu"
+steps: 30
+model_name: "llama3-70b"
+enable_checkpointing: False
+attention: "cudnn_flash_te"
+dcn_data_parallelism: -1
+dcn_fsdp_parallelism: 1
+dcn_pipeline_parallelism: 1
+dcn_tensor_parallelism: 1
+dcn_sequence_parallelism: 1
+ici_fsdp_parallelism: 8
+ici_data_parallelism: 1
+ici_sequence_parallelism: 1
+ici_tensor_parallelism: 1
+ici_pipeline_parallelism: 1
+ 
+remat_policy: 'full'
+optimizer_memory_host_offload: False
+param_scan_axis: 1
+ 
+use_iota_embed: True
+scan_layers: True
+
+profiler: ""
+
+async_checkpointing: False
+logits_dot_in_fp32: False
+megablox: False
+dtype: "bfloat16"
+quantization: ""
+quantize_kvcache: False
+kv_quant_axis: "heads_and_dkv"
+kv_quant_dtype: "int8"
+weight_dtype: bfloat16
+checkpoint_is_quantized: False # Set to True if reading from a saved aqt quantized checkpoint
+per_device_batch_size: 7
+max_target_length: 8192
+hf_path: "parquet" 
+hf_train_files: "/home/amd/data/c4/en/partial-train/000*.parquet"
+dataset_type: "hf"
+tokenizer_path: "/home/amd/data/Llama-2-7b-hf/"' > $OUTPUT_DIR/llama3_70b_gpu.yml
+
+#If podman is available instead of docker, then you need this export otherwise
+#comment the below line and uncomment the line after that
+#export docker=podman
+docker=docker
+
+
+
+# get the test data
+pip install -U "huggingface_hub[cli]" > /dev/null
+export PATH=~/.local/bin/:$PATH
+huggingface-cli download meta-llama/Llama-2-7b-chat-hf --local-dir $HOME/data/Llama-2-7b-hf/ --cache-dir $HOME/data > /dev/null
+huggingface-cli download legacy-datasets/c4 --include "*.parquet" --repo-type dataset --local-dir $HOME/data/c4 --revision refs/convert/parquet --cache-dir $HOME/data > /dev/null
+
+
+$docker run --rm --privileged --network host --device /dev/dri --device /dev/kfd \
+  --cap-add=IPC_LOCK --volume /dev/infiniband:/dev/infiniband \
+  -v $HOME:$HOME -v $HOME/data:/home/amd/data --tmpfs /dev/shm:size=50G \
+  --mount type=bind,source=$OUTPUT_DIR,target=/workspace/maxtext/output \
+  -w /workspace/maxtext $IMAGE /bin/bash -c "
+        set -e
+        echo \"Running Llama-3-70b\"
+	echo '${IMAGE}'
+	cp $OUTPUT_DIR/maxtext_env_70b.sh .
+	cp $OUTPUT_DIR/llama3_70b_gpu.yml MaxText/configs/llama3_70b_gpu.yml
+        source maxtext_env_70b.sh	
+        python MaxText/train.py MaxText/configs/llama3_70b_gpu.yml  2>&1 |& tee -a llama3_70b.real.log
+    "
+
